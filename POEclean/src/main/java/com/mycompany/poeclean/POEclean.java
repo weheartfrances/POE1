fcf78cf38c8d85c9055b61dacb2033f402ce1fcb
Frances <weheartfrances@gmail.com>
2026-04-14 19:39:09 +0200

    Recreated cleaner version

---
+++ POEclean/src/main/java/com/mycompany/poeclean/POEclean.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poeclean;
+
+/**
+ *
+ * @author frances
+ */
+public class POEclean {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
