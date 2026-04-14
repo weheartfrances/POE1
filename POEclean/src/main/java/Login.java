6920b58fadc1d593d0a633983dd32b2ba348968f
Frances <weheartfrances@gmail.com>
2026-04-14 20:40:36 +0200

    Login class has been created as well as Constructor!!

---
+++ POEclean/src/main/java/Login.java
@@ -0,0 +1,19 @@
+
+public class Login {
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+    private String firstName;
+    private String lastName;
+    
+// Constructor added for better personal understanding
+    
+    public Login(String username, String password, String cellPhoneNumber, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName; }
+    
+    public
+}
