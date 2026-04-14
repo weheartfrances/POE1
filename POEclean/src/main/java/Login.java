1eb8556b1e9d818dbd8d91b7aaac450ff70fdd34
Frances <weheartfrances@gmail.com>
2026-04-14 21:13:26 +0200

    main class created!

--- POEclean/src/main/java/Login.java
+++ POEclean/src/main/java/Login.java
@@ -2,18 +2,13 @@
 public class Login {
     private String username;
     private String password;
-    private String cellPhoneNumber;
-    private String firstName;
-    private String lastName;
-    
+  
 // Constructor added for better personal understanding
     
-    public Login(String username, String password, String cellPhoneNumber, String firstName, String lastName) {
+    public Login(String username, String password) {
         this.username = username;
         this.password = password;
-        this.cellPhoneNumber = cellPhoneNumber;
-        this.firstName = firstName;
-        this.lastName = lastName; }
+    }
     
     public boolean checkUsername() { 
     return username.contains("-") && username.length() <= 5;
