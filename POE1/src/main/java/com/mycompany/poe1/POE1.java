dcf0976019a3d6908983f5bd0ff2c8a8ebe3eb81
Frances <weheartfrances@gmail.com>
2026-04-14 17:31:04 +0200

    Initial Project Setup!

---
+++ POE1/src/main/java/com/mycompany/poe1/POE1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poe1;
+
+/**
+ *
+ * @author frances
+ */
+public class POE1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
